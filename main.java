4f4e2e2359dbdf16af482aec5e30d2c76d72e324
Вадим Карзаков <vkarzakov31@gmail.com>
2023-11-30 22:12:26 +0300

    add if i

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 class Main{
     public static void main(String[] args) {
+        int i=0;
+        if (i<10){
+            i=i+1;
+        }
         System.out.print("Это добавлено в ветке dev");
     }
 }
